17d0600b28846e8c92659a6b3c7dfe6c22deb2e2
azimnorqulov <azim.norqulov@icloud.com>
2026-02-08 23:25:14 +0500

    carousel add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,17 @@
 import { NavLink } from "react-router-dom";
 import Rings from "../assets/wedding-ring.png";
 import Flower from "../assets/flower.png";
+import Animation from "../assets/animation.png";
+import RingAnimation from "../assets/wedding-rings.png";
+
+const createAnimations = () =>
+  Array.from({ length: 20 }).map(() => ({
+    left: Math.random() * 100,
+    delay: Math.random() * 6,
+    duration: 5 + Math.random() * 5,
+  }));
+
+const ANIMATIONS = createAnimations();
 
 const Header = () => {
   return (
@@ -8,6 +19,40 @@ const Header = () => {
       <div className="container">
         <nav className="navbar">
           <ul className="nav-list">
+            <img
+              src={Animation}
+              alt="Animation"
+              className="floating-animation"
+            />
+
+            {ANIMATIONS.map((anim, i) => (
+              <img
+                key={i}
+                src={Animation}
+                alt="animation"
+                className="floating-animation"
+                style={{
+                  left: `${anim.left}%`,
+                  animationDelay: `${anim.delay}s`,
+                  animationDuration: `${anim.duration}s`,
+                }}
+              />
+            ))}
+
+            {/* {ANIMATIONS.map((anim, i) => (
+              <img
+                key={i}
+                src={RingAnimation}
+                alt="animation"
+                className="floating-animation"
+                style={{
+                  left: `${anim.left}%`,
+                  animationDelay: `${anim.delay}s`,
+                  animationDuration: `${anim.duration}s`,
+                }}
+              />
+            ))} */}
+
             <li>
               <NavLink to="/" end>
                 Home
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,52 @@
-import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectFade } from "swiper/modules"; // ← add EffectFade
 
-const Home = () => {
+import ToyImg from "../assets/toy1.jpg";
+import ToyImg2 from "../assets/toy2.jpg";
+import ToyImg3 from "../assets/toy3.jpg";
+
+import "swiper/css";
+import "swiper/css/effect-fade";
+
+export default function TestSwiper() {
   return (
-    <div className="home-container">
-      <h1>Home Page</h1>
-      <p>Welcome to our wedding website!</p>
+    <div className="img-container" style={{ height: "500px" }}>
+      <Swiper
+        modules={[Autoplay, EffectFade]}
+        loop={true}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        speed={1200}
+        effect="fade"
+        fadeEffect={{
+          crossFade: true,
+        }}
+        style={{ height: "100%" }}
+      >
+        <SwiperSlide>
+          <img
+            src={ToyImg}
+            alt="Toy 1"
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={ToyImg3}
+            alt="Toy 3"
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={ToyImg2}
+            alt="Toy 2" // ← fixed alt text (was "Toy 1")
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+          />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
-};
-
-export default Home;
+}
